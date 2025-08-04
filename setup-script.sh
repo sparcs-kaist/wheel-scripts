87cd2fcb1c421a1c4dd8bca05f29aa5876ba128d
Kwabang <kwabang2827@gmail.com>
2025-08-04 18:41:16 +0900

    feat: Enhance setup script with additional package installations, user/group cleanup, SSH configuration, and timezone settings

--- setup-script.sh
+++ setup-script.sh
@@ -72,17 +72,53 @@ fi
 
 # Install basic packages
 echo "=== Installing Basic Packages ==="
-apt-get update
-apt-get install -y git vim curl screen htop iftop ca-certificates nginx p7zip-full unzip zip
+apt update
+apt install -y git vim curl screen htop iftop ca-certificates nginx p7zip-full unzip zip qemu-guest-agent resolvconf
+
+# Set nameserver to 1.1.1.1
+echo "=== Set nameserver to 1.1.1.1 ==="
+mv /etc/resolv.conf /etc/resolv.conf.backup
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
 
 # Setup default editor
 echo "=== Configuring Default Editor ==="
 update-alternatives --set editor /usr/bin/vim.basic
 
+# Clear Ubuntu default accounts
+sed -i 's|^ENV_PATH[[:space:]]*PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games|ENV_PATH\tPATH=/usr/local/bin:/usr/bin:/bin|' /etc/login.defs
+
+names=(
+  "lp" "mail" "games" "dialout" "fax" "news" "uucp" "proxy" "voice"
+  "sasl" "plugdev" "users" "cdrom" "floppy" "tape" "audio"
+  "dip" "irc" "src" "list" "gnats" "swtpm" "input" "staff" "backup"
+  "sgx" "rdma"
+)
+for name in "\${names[@]}"; do
+  # Check if the user exists and remove if it does
+  if /usr/bin/id -u "\$name" &>/dev/null; then
+    echo "Removing user: \$name"
+    /usr/sbin/userdel -f "\$name"
+  fi
+  # Check if the group exists and remove if it does
+  if /usr/bin/getent group "\$name" &>/dev/null; then
+    echo "Removing group: \$name"
+    /usr/sbin/groupdel -f "\$name"
+  fi
+   sed -i "/\$name/d"  /usr/lib/sysusers.d/basic.conf
+done
+
+# Disable ssh root login
+printf "PermitRootLogin no\nPubkeyAuthentication yes\nPasswordAuthentication yes\n" > /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+
+# Setting timezone to KST
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+echo "Asia/Seoul" | tee /etc/timezone
+dpkg-reconfigure tzdata
+
 # Setup docker-ce
 if $INSTALL_DOCKER; then
   echo "=== Installing Docker ==="
-  for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
+  for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt remove $pkg; done
   install -m 0755 -d /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   chmod a+r /etc/apt/keyrings/docker.asc
@@ -91,9 +127,9 @@ if $INSTALL_DOCKER; then
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt-get update
+  apt update
 
-  apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   systemctl enable docker.socket
 fi
 
@@ -114,3 +150,8 @@ rm /usr/lib/python3.12/EXTERNALLY-MANAGED
 curl https://bootstrap.pypa.io/get-pip.py | python3
 python3 -m pip install -U pip wheel setuptools
 python3 -m pip install -U certbot certbot-nginx
+
+# Cleanup
+apt clean all
+history -c
+exit
